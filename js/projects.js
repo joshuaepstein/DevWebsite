9fdf88e851bfdd397d982cf8ae66f1e9ec26fc74
joshuaepstein <joshsepstein@gmail.com>
2022-03-20 00:45:11 +0000

    Change Project Title using ?project=

--- js/projects.js
+++ js/projects.js
@@ -2,6 +2,7 @@ $(window).on("load", function(){
     if(getParameterByName('project') === "ultrautils" || getParameterByName('project') === "wordle" || getParameterByName('project') === "jebot"){
         $("#default").hide()
         $("#" + getParameterByName('project')).show()
+        document.title = getParameterByName('project') + " | Dev JoshuaEpstein"
     } else if(getParameterByName('project')){
         $("#doesnt-exist").text("This project does not exist, view the available projects below");
     }
